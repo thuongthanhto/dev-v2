eaa45ad380a038ecd57e15639016856025213b77
Thuong To <thuongthanhto@gmail.com>
2024-10-21 16:52:10 +0700

    Update contract address

--- src/lib/config/sepolia/tokenlists.ts
+++ src/lib/config/sepolia/tokenlists.ts
@@ -3,7 +3,7 @@ import { TokenListURLMap } from '@/types/TokenList';
 const tokenlists: TokenListURLMap = {
   Balancer: {
     Allowlisted:
-      'https://raw.githubusercontent.com/balancer/tokenlists/main/generated/balancer.tokenlist.json',
+      'https://github.com/thuongthanhto/token-list/blob/main/tokenlist.json',
   },
   External: [],
 };
